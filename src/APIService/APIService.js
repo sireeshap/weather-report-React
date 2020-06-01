a1afeffe05d1c75816b180d5f333b789f3be15e3
Sireesha <sireeshap@users.noreply.github.com>
2020-06-01 16:43:47 +0530

    code reduction

--- src/APIService/APIService.js
+++ src/APIService/APIService.js
@@ -1,19 +1,15 @@
 /* Description: This will make API calls to server and will return data to components */
 export default async function APIService(cityelement) {
     try {
-      let response='';
-      /* In real time we need to set api string and API token in environment variable in .env file. 
+        let response = '';
+        /* In real time we need to set api string and API token in environment variable in .env file.
       then below line will help to hide confidential information*/
-      // let result = await fetch(process.env.REACT_APP_API_URL + cityelement + '&appid='+REACT_APP_API_URL);
-        
-      let result = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityelement + '&appid=f9b14f353ab97350fc4e49e9773415da');
-        if (result.ok) {
-            response = await result.json()
-            return response
-        } else {
-            response = await result.json()
-            return response
-        }
+        // let result = await fetch(process.env.REACT_APP_API_URL + cityelement +
+        // '&appid='+REACT_APP_API_URL);
+
+        let result = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityelement + '&appid=f9b14f353ab97350fc4e49e9773415da');
+        response = await result.json()
+        return response
     } catch (error) {
         throw error
     }
